6a8ce084ac4df67c0bde87dae327a741d6a42685
waldry <waldry@DESKTOP-UA2OP50>
2019-11-30 22:55:50 -0400

    Hay que ejecutar el server por separado.

--- Fabrica de Queso/src/server/Server.java
+++ Fabrica de Queso/src/server/Server.java
@@ -1,94 +1,58 @@
 package server;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-public class Server extends Thread{
-	private static ArrayList<String> salida = new ArrayList<>();
-	public String threadName;
-	private Thread t;
-	private boolean firstLine = true;
-	private String titulo = "";
-	
-	public Server(String name) {
-		threadName = name;
-		System.out.println("Configurando el servidor: "+ threadName);
-	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		System.out.println("Configurando Almacenamiento de archivos");
-		iniciar();
-		System.out.println("Configuracion Almacenamiento de archivos completada");
-	}
-	public void start(){
-		System.out.println("Creando sistemas de configuracion del servidor.");
-		if (t == null) {
-			t = new Thread(this, threadName);
-			t.start();
-		}
-	}
-	private void iniciar() {
-		try {
-		System.out.println("LocalHost" + InetAddress.getLocalHost().toString());
-			
-		} catch (UnknownHostException e) {
-			System.out.println("Hubo un error al intentar acceder a la ip local del equipo" + e);
-		}
-		ServerSocket socket = null;
-		try {
-			socket = new ServerSocket(3000);
-		} catch (IOException e) {
-			System.out.println("El puerto: "+ socket+ "Esta ocupado.");
-			System.exit(1);
-		}
-		while(true) {
-			String entrada = "";
-			try {
-				Socket sock = socket.accept();
-				BufferedReader lector = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-				String currentLine;
-				while ((currentLine = lector.readLine()) != null) {
-					entrada += currentLine + "\n";
-					if(firstLine) {
-						titulo = entrada.replace("ID: ","");
-						titulo = titulo.replace("\n", "");
-						firstLine = false;
-					}
-					//System.out.println(currentLine);
-				}
-				
-				firstLine = true;
-				salida.add(entrada);
-				//dos.writeBytes("Recibido" + 'n');		
-				if(sock.isInputShutdown() && sock.isOutputShutdown()) {
-					//dos.close();
-					sock.close();
-					lector.close();	
-				}
-			} catch (Exception e) {
-				System.out.println("Hubo un maldito error");
-			}
-		}
-		
-	}
-	
-	public void imprimir(){
-		
 
-		for(String factura : salida) {
-			try {
-				
-				String directorio = new File(".").getCanonicalPath() + "\\src\\"+"factura-" + titulo +".txt";
-				//System.out.println("\n"+factura);
-				File texto = new File(directorio);
-				FileWriter writer = new FileWriter(texto);
-				BufferedWriter bw = new BufferedWriter(writer);
-				bw.write(factura);
-				bw.close();
-			}catch(IOException e) {
-				System.err.format("Error escribiendo el archivo" + e);
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.*;
+
+import logico.Empresa;
+public class Server extends Thread{
+	private static DataInputStream entrada;
+	private static DataOutputStream salida;
+	private static DataInputStream entradaF;
+	private static DataOutputStream salidaF;
+	private static Socket soc;
+	private static Socket soc2;
+	
+	public static void main(String[] args) {
+		try {
+			int port = 3002;
+			ServerSocket soc = new ServerSocket(port);
+			while(true) {
+				Setsockets(soc.accept());
+				System.out.println("Esto me tiene cansado.");
+				String name = entrada.readUTF();
+				System.out.println(name);
+				File ficheroSalida = new File("Facts/"+name+".txt");
+				receiveFile(ficheroSalida);
+				System.out.println("Archivo" +name+"Guardado correctamente.");
 			}
-		}	
-	 }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static  void Setsockets (Socket soc) throws IOException {
+		soc = soc;
+		soc2 = soc;
+		entrada = new DataInputStream(soc.getInputStream());
+		salida = new DataOutputStream(soc.getOutputStream());
+		entradaF = new DataInputStream(soc2.getInputStream());
+		salidaF = new DataOutputStream(soc2.getOutputStream());
+	}
+	public static void receiveFile(File file) throws IOException {
+		FileOutputStream fileOut = new FileOutputStream(file);
+//  convierte el fichero en bytes.
+		byte[] buf = new byte[Short.MAX_VALUE];
+		int bytesSent;        
+		while( (bytesSent = entrada.readShort()) != -1 ) {
+			entrada.readFully(buf,0,bytesSent);
+			fileOut.write(buf,0,bytesSent);
+		}
+		fileOut.close();
+
+
+	} 
 }
